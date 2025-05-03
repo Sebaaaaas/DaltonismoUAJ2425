3796c03418616e3e6b49c2b35015e913158a69b3
Sebaaaaas <sebsan01@ucm.es>
2025-05-03 19:11:26 +0200

    Fondo transparente, funciona .exe

---
+++ DaltonismoVS/DaltonismoVS/DaltonismoVS/CaptureScreen.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Composition;
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+
+
+
+namespace DaltonismoVS
+{
+    internal class CaptureScreen
+    {
+
+        private GraphicsCaptureItem _item;
+        private Direct3D11CaptureFramePool _framePool;
+        private CanvasDevice _canvasDevice;
+        private GraphicsCaptureSession _session;
+        public void CaptureCurrentScreen()
+        {
+            Console.WriteLine("Nice screen");
+            //_session.StartCapture();
+        }
+
+        public void StartCaptureInternal(GraphicsCaptureItem item)
+        {
+            _item = item;
+
+            _framePool = Direct3D11CaptureFramePool.Create(
+                _canvasDevice, // D3D device
+                Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized, // Pixel format
+                2, // Number of frames
+                _item.Size); // Size of the buffers
+
+            _session = _framePool.CreateCaptureSession(_item); // Conseguimos una referencia al GraphicsCaptureSession
+        }
+
+
+        // Esta función en teoría pide al usuaruui que indique la pantalla a capturar, y comienza a capturar si elige algo válido
+        public async Task StartCaptureAsync()
+        {
+            // The GraphicsCapturePicker follows the same pattern the
+            // file pickers do.
+            var picker = new GraphicsCapturePicker(); // Esto llama a la IU para que el usuario seleccione la pantalla para sacar capturas
+            GraphicsCaptureItem item = await picker.PickSingleItemAsync();
+
+            // The item may be null if the user dismissed the
+            // control without making a selection or hit Cancel.
+            if (item != null)
+            {
+                StartCaptureInternal(item);
+            }
+        }
+    }
+
+}
--- DaltonismoVS/DaltonismoVS/DaltonismoVS/MainWindow.xaml.cs
+++ DaltonismoVS/DaltonismoVS/DaltonismoVS/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Hosting;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
@@ -10,8 +12,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.Storage;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Composition;
+using Windows.Graphics.DirectX;
+using Windows.UI.Composition;
+
+using System.Numerics;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +36,29 @@ namespace DaltonismoVS
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        CaptureScreen cs;
         public MainWindow()
         {
             this.InitializeComponent();
+            cs = new CaptureScreen();
+        }
+
+        // Cuando se inicializa el programa se llama aquí(creo que no se llama actualmente)
+        public void OnInitialization()
+        {
+            if (!GraphicsCaptureSession.IsSupported())
+            {
+                // Hide the capture UI if screen capture is not supported.
+                //CaptureButton.Visibility = Visibility.Collapsed;
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button1.Content = "Daltonico detectado";
+            cs.CaptureCurrentScreen();
         }
+
     }
+
 }
